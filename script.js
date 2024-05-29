733a7d4d2861829911c08b35b6ecc384f61c52be
Ruizhe Lai <rulai@student.unimelb.edu.au>
2024-05-29 15:08:09 +0800

    header & main

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function footerToggle(footerBtn) {
     $(footerBtn).toggleClass("btnActive");
     $(footerBtn).next().toggleClass("active");
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
